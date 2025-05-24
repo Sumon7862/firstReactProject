155309ab3a54caa811bc4e0ade3fde6cc7356814
Sumon7862 <mdsumonmia383@gmail.com>
2025-05-25 04:04:22 +0600

    Finally Done

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -3,8 +3,12 @@ import { FcGoogle } from "react-icons/fc";
 import { FaSquareGithub } from "react-icons/fa6";
 import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";
+import { useState } from "react";
 
 const Login = () => {
+
+  const [pass, setPass] = useState(false);
+
   return (
     <>
       <div className="flex items-center justify-center bg-green-950 h-[100vh]">
@@ -19,14 +23,22 @@ const Login = () => {
               id=""
               required
             />
-            <input
-              type="password"
-              className="w-full border-1 px-4 py-2 rounded focus:ring-2 focus:ring-green-950 focus:border-0"
-              placeholder="Password"
-              name=""
-              id=""
-              required
-            />
+            {/* Only Password */}
+            <div className="relative flex items-center">
+              <input
+                type={pass ? 'text' : 'password'}
+                className="w-full border-1 px-4 py-2 rounded focus:ring-2 focus:ring-green-950 focus:border-0"
+                placeholder="Password"
+                name=""
+                id=""
+                required
+              />
+              {
+                pass ? <IoEyeOff onClick={() => setPass(!pass)} className="absolute right-3 cursor-pointer"/> 
+                : 
+                <IoEye onClick={() => setPass(!pass)} className="absolute right-3 cursor-pointer"/>
+              }
+            </div>
 
             <p className="text-right font-medium cursor-pointer hover:text-green-950 hover:underline">
               Forget Password
