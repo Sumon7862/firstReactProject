a6b233df1614b12f075b1e2aba1034f2a4ba4c78
Sumon7862 <mdsumonmia383@gmail.com>
2025-05-24 15:38:41 +0600

    again

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,7 @@
 const HeroSection = () => {
     return (
         <>
-            <div className="flex flex-col items-center justify-center text-white px-4 h-[90vh]" 
-            style={{
-                backgroundImage: "url('../assets/background.jpg')",
-                backgroundSize: 'cover',
-                backgroundPosition: 'center'
-
-            }}>
+            <div className="flex flex-col items-center justify-center text-white px-4 h-[90vh] bg-[url(./src/assets/background.jpg)] bg-cover bg-center">
                 <h2 className=" text-2xl md:text-6xl mb-4 font-bold">Welcome to my website.</h2>
                 <p className="text-lg md:text-xl mb-4 font-medium">Your journey start here.</p>
                 <button className="text-indigo-600 bg-white px-6 py-2 rounded-md font-medium cursor-pointer hover:bg-gray-200">Purchase product</button>
